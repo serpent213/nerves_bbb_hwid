77b1b7e21426b1ee176ef5600c323e62cc27de98
Steffen Beyer <steffen@beyer.io>
2017-08-05 19:36:52 +0200

    fresh mix project

---
+++ lib/nerves_bbb_hwid.ex
@@ -0,0 +1,18 @@
+defmodule NervesBbbHwid do
+  @moduledoc """
+  Documentation for NervesBbbHwid.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesBbbHwid.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_bbb_hwid_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesBbbHwidTest do
+  use ExUnit.Case
+  doctest NervesBbbHwid
+
+  test "greets the world" do
+    assert NervesBbbHwid.hello() == :world
+  end
+end
